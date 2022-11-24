fc432093a6a8fe830cdc4d4d7307b63534f1b876
Exclusivefrog28 <deee4444@gmail.com>
2022-11-24 19:39:32 +0100

    FAB-Dialog refactor

---
+++ components/LocationDialog.js
@@ -0,0 +1,89 @@
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Dialog, Button, TextInput, IconButton, Searchbar, Divider, List } from 'react-native-paper';
+import { LocationContext } from '../context/LocationContext';
+import GetLocation from 'react-native-get-location';
+import axios from 'axios';
+import SplashScreen from 'react-native-splash-screen';
+
+
+const LocationDialog = ({ isVisible, hide }) => {
+
+    const { location, setLocation } = useContext(LocationContext);
+    const [locCandidate, setLocCandidate] = useState(location);
+    const [cities, setCities] = useState([]);
+    const [searchLoading, setSearchLoading] = useState(false);
+
+    const renderItem = ({ item }) => (
+        <CityItem item={item} />
+    );
+
+    const CityItem = ({ item }) => (
+        <List.Item
+            title={item.name}
+            description={item.country}
+            left={props => <List.Icon {...props} icon="earth" />}
+            onPress={() => setLocCandidate({ lat: item.latitude.toFixed(2), long: item.longitude.toFixed(2) })}
+        />
+    );
+
+    const search = query => {
+        setSearchLoading(true);
+        axios.get('https://geocoding-api.open-meteo.com/v1/search?name=' + query + '&count=3').then(
+            (response) => {
+                setCities(response.data.results);
+                setSearchLoading(false);
+            }
+        );
+    };
+
+    const save = () => {
+        if (location !== locCandidate) {
+            setLocation(locCandidate);
+            SplashScreen.show();
+        }
+        hide();
+    };
+
+    return (
+        <Dialog visible={isVisible} onDismiss={hide} dismissable={false}>
+            <Dialog.Title>Location Settings</Dialog.Title>
+            <Dialog.Content >
+                <View style={styles.coordinates}>
+                    <TextInput label="Latitude" mode="outlined" value={locCandidate.lat}
+                        onChangeText={text => setLocation({ lat: text, long: locCandidate.long })} style={styles.textInput} />
+                    <TextInput label="Longitude" mode="outlined" value={locCandidate.long}
+                        onChangeText={text => setLocation({ lat: locCandidate.lat, long: text })} style={styles.textInput} />
+                    <IconButton mode="contained" icon="crosshairs-gps" size={28} style={styles.locationButton} labelStyle={styles.locationIcon}
+                        onPress={() => {
+                            GetLocation.getCurrentPosition({ enableHighAccuracy: false, timeout: 15000 })
+                                .then(newLocation => { setLocCandidate({ lat: parseFloat(newLocation.latitude).toFixed(4), long: parseFloat(newLocation.longitude).toFixed(4) }); console.log(newLocation); })
+                                .catch(error => { console.warn(error); });
+                        }} />
+                </View>
+                <Divider />
+                <View style={styles.placename}>
+                    <Searchbar placeholder="Search by city name" onChangeText={search} loading={searchLoading} />
+                    <FlatList
+                        keyboardShouldPersistTaps="handled"
+                        data={cities}
+                        extraData={cities}
+                        renderItem={renderItem} />
+                </View>
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={hide}>Cancel</Button>
+                <Button onPress={save}>Save</Button>
+            </Dialog.Actions>
+        </Dialog>
+    );
+};
+
+const styles = new StyleSheet.create({
+    coordinates: { flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginVertical: 10 },
+    textInput: { flex: 3, marginHorizontal: 5 },
+    locationButton: { flex: 1 },
+    city: {},
+});
+
+export default LocationDialog;
--- components/Settings.js
+++ components/Settings.js
@@ -1,9 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import { Portal, FAB, Dialog, Button, TextInput, IconButton, Searchbar, Divider, List } from 'react-native-paper';
-import { LocationContext } from '../context/LocationContext';
-import GetLocation from 'react-native-get-location';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { FAB } from 'react-native-paper';
+import LocationDialog from './LocationDialog';
 
 const Settings = () => {
 
@@ -15,35 +12,6 @@ const Settings = () => {
   const showLocDialog = () => setLocVisible(true);
   const hideLocDialog = () => setLocVisible(false);
 
-  const { location, setLocation } = useContext(LocationContext);
-
-  const [cities, setCities] = useState([]);
-
-  const [searchLoading, setSearchLoading] = useState(false);
-
-  const renderItem = ({ item }) => (
-    <CityItem item={item}/>
-  );
-
-  const CityItem = ({ item }) => (
-    <List.Item
-      title={item.name}
-      description={item.country}
-      left={props => <List.Icon {...props} icon="earth"/>}
-      onPress={() => setLocation({lat: item.latitude.toFixed(2), long:item.longitude.toFixed(2)})}
-    />
-  );
-
-  const search = query => {
-    setSearchLoading(true);
-    axios.get('https://geocoding-api.open-meteo.com/v1/search?name=' + query + '&count=3').then(
-      (response) => {
-        setCities(response.data.results);
-        setSearchLoading(false);
-      }
-    );
-  };
-
   return (
     <>
       <FAB.Group
@@ -52,48 +20,17 @@ const Settings = () => {
         variant="secondary"
         icon="cog"
         actions={[
+          { icon: 'information-outline', label: 'About' },
+          { icon: 'dots-horizontal', label: 'Preferences' },
           { icon: 'map-marker-outline', label: 'Location', onPress: showLocDialog },
+
         ]}
         onStateChange={onStateChange} />
-      <Portal>
-        <Dialog visible={locVisible} onDismiss={hideLocDialog}>
-          <Dialog.Title>Location Settings</Dialog.Title>
-          <Dialog.Content >
-            <View style={styles.coordinates}>
-              <TextInput label="Latitude" mode="outlined" value={location.lat}
-                onChangeText={text => setLocation({ lat: text, long: location.long })} style={styles.textInput} />
-              <TextInput label="Longitude" mode="outlined" value={location.long}
-                onChangeText={text => setLocation({ lat: location.lat, long: text })} style={styles.textInput} />
-              <IconButton mode="contained" icon="crosshairs-gps" size={28} style={styles.locationButton} labelStyle={styles.locationIcon}
-                onPress={() => {
-                  GetLocation.getCurrentPosition({ enableHighAccuracy: false, timeout: 15000 })
-                  .then(newLocation => { setLocation({ lat: parseFloat(newLocation.latitude).toFixed(4), long: parseFloat(newLocation.longitude).toFixed(4) }); console.log(newLocation); })
-                  .catch(error => { console.warn(error); });
-                }} />
-            </View>
-            <Divider />
-            <View style={styles.placename}>
-              <Searchbar placeholder="Search by city name" onChangeText={search} loading={searchLoading} />
-              <FlatList
-                data={cities}
-                extraData={cities}
-                renderItem={renderItem} />
-            </View>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hideLocDialog}>Done</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
+      <LocationDialog
+        isVisible={locVisible}
+        hide={hideLocDialog} />
     </>
   );
 };
-
-const styles = new StyleSheet.create({
-  coordinates: { flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginVertical: 10 },
-  textInput: { flex: 3, marginHorizontal: 5 },
-  locationButton: { flex: 1 },
-  city: {},
-});
 
 export default Settings;
